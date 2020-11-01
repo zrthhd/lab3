cf904673cc5354e8a2d6dec177f65f5e0fb29c61
zrthhd <2020978757@isiswa.edu.my>
2020-11-01 22:31:00 +0800

    create modify code

--- 3.5.c
+++ 3.5.c
@@ -8,18 +8,14 @@
 int getNum(int num)
 {
 	int pipefds[2];
-/*	int num;*/
 	printf("Please enter a number: ");
 	scanf("%d", &num);
 
-/*	close(pipefds[0]);
-	write(pipefds[1], num, NUM_LENGTH + 1); */
-
-	write(pipefds[1], &num, NUM_LENGTH + 1);
 	close(pipefds[1]);
+	write(pipefds[1], num, NUM_LENGTH + 1); 
+
 	read(pipefds[0], &num, NUM_LENGTH + 1);
-	close(pipefds[0]);
-	return &num;
+
 }
 int main(void)
 {
@@ -31,46 +27,25 @@ int main(void)
 	if(pid == 0)
 	{
 		getNum(num);
+		if(num %2 != 0)
+		{
+			printf("%d is a prime number. \n", num);
+					
+		}
+
+		else
+		{
+
+			printf("%d is not a prime number. \n",num);
+			
+		}
 		exit (EXIT_SUCCESS);
 	}
 
-	else if(pid > 0)
+	 if(pid > 0)
 	{
 		wait(NULL);
 
-		getNum(num);
-	/*	close(pipefds[1]);
-		read(pipefds[0], buffer, NUM_LENGTH + 1);*/ 
-		int i, flag = 0;
-
-	/*	int num = 0 ;*/
-		for (int i = 2; i <= num / 2; ++i)
- 		{
-
-        			if (num % i == 0) 
-				{
-            				flag = 1;
-            				break;
-        			}
-    
-
-				if (num == 1) 
-				{
-        				printf("1 is neither prime nor composite.");
-  				}
-    				else 
-				{
-        				if (flag == 0)
-            				printf("%d is a prime number.", num);
-        				else
-           				printf("%d is not a prime number.", num);
-    				}
-		}
-
-
-		
-
-	/*	close(pipefds[0]);*/
 	}
 
 	return EXIT_SUCCESS;
